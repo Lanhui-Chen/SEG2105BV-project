13dd369607b77f98b355f410997907e396dc6c10
Lanhui-Chen <lchen245@uottawa.ca>
2022-10-20 09:02:04 -0400

    added logout function and changed default activity
    - changed the AndroidManifest.xml file so that login activity now become the default open activity when app starts.
    
    - Also added logout function

--- Mealer/app/src/main/java/com/example/mealer/activity_login_successful.java
+++ Mealer/app/src/main/java/com/example/mealer/activity_login_successful.java
@@ -2,7 +2,10 @@ package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_login_successful extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class activity_login_successful extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_successful);
+
+        final Button logOut = findViewById(R.id.logout);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_login_successful.this, activity_login.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
     }
 }
