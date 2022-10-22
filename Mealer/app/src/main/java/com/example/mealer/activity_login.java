74895328eab061a7f3d70de50dea7b74a8736ff1
Lanhui-Chen <lchen245@uottawa.ca>
2022-10-21 23:55:08 -0400

    added some functions in login and register

--- Mealer/app/src/main/java/com/example/mealer/activity_login.java
+++ Mealer/app/src/main/java/com/example/mealer/activity_login.java
@@ -1,8 +1,31 @@
 package com.example.mealer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class activity_login extends AppCompatActivity {
 
@@ -10,5 +33,153 @@ public class activity_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText emailEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final Button moveRegisterButton = findViewById(R.id.moveRegister);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final boolean flag = false;
+
+        loginButton.setEnabled(flag);
+
+        emailEditText.setError("Not a valid email");
+        passwordEditText.setError("Password incorrect");
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(isEmailValid(emailEditText.toString()) == isPasswordValid(passwordEditText.toString()));
+            }
+        };
+        emailEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginVerification();
+                }
+                return false;
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginVerification();
+            }
+        });
+
+
+        moveRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(activity_login.this, activity_register.class));
+            }
+        });
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean isPasswordValid(String password) {
+        return password != null && password.trim().length() >= 5;
+    }
+
+    public void loginVerification(){
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+        loadingProgressBar.setVisibility(View.VISIBLE);
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users");
+        final String username;
+
+        //Checks whether the imputed username and password are valid in the firebase database
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                boolean found = false;
+                String tmpUsername = "";
+                Iterator<DataSnapshot> children = snapshot.getChildren().iterator();
+                while(!found && children.hasNext()){
+
+                    tmpUsername = children.next().getKey();
+                    if(tmpUsername.toLowerCase().equals(usernameEditText.getText().toString().toLowerCase())){
+
+                        found = true;
+
+                    }
+
+                }
+                if(found){
+
+                    passwordVerification(tmpUsername);
+                }
+
+                else{
+
+                    usernameEditText.setError(getString(R.string.Nonexistent_email));
+                    passwordEditText.setError(null);
+                    loadingProgressBar.setVisibility(View.INVISIBLE);
+
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void passwordVerification(String username){
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final String userName = username;
+
+        DatabaseReference userPassRef = FirebaseDatabase.getInstance().getReference("users/" + username + "/password");
+
+        userPassRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(passwordEditText.getText().toString().equals(snapshot.getValue(String.class))){
+                    Intent intent = new Intent(activity_login.this, activity_login_successful.class);
+                    intent.putExtra("EXTRA_USERNAME", userName);
+                    usernameEditText.setText("");
+                    passwordEditText.setText("");
+                    startActivity(intent);
+                } else {
+                    passwordEditText.setError(getString(R.string.wrong_password));
+                    usernameEditText.setError(null);
+                }
+                loadingProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
     }
 }
--- Mealer/app/src/main/java/com/example/mealer/activity_register.java
+++ Mealer/app/src/main/java/com/example/mealer/activity_register.java
@@ -3,12 +3,82 @@ package com.example.mealer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class activity_register extends AppCompatActivity {
+
+    private String selectedRole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText firstname = findViewById(R.id.firstname);
+        final EditText lastname = findViewById(R.id.lastname);
+        final EditText emailEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final EditText address = findViewById(R.id.address);
+        final RadioGroup roleRadio = findViewById(R.id.radioGroupRole);
+        final RadioButton lastRadioButton = findViewById(R.id.radioButtonClient);
+        final EditText ccard = findViewById(R.id.ccard);
+        final EditText description = findViewById(R.id.description);
+        final Button register = findViewById(R.id.register);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final boolean flag = false;
+
+        register.setEnabled(flag);
+
+        firstname.setError("Enter firstname");
+        lastname.setError("Enter lastname");
+        emailEditText.setError("Not a valid email");
+        passwordEditText.setError("Password incorrect");
+        address.setError("Enter address");
+        ccard.setError("Enter a valid credit card number");
+
+
+        roleRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+                    case R.id.radioButtonCook:
+                        selectedRole = "Cook";
+                        ccard.setVisibility(View.VISIBLE);
+                        lastRadioButton.setError(null);
+                        register.setEnabled(true);
+                        break;
+                    case R.id.radioButtonClient:
+                        selectedRole = "Client";
+                        description.setVisibility(View.VISIBLE);
+                        lastRadioButton.setError(null);
+                        register.setEnabled(true);
+                        break;
+
+                }
+
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
